937babb7d3aa0ba779311f6e734e0842202e2e12
Mahdi Hosseini <mihi2000@gmail.com>
2020-01-04 20:51:41 -0800

    add ksql module

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -6,31 +6,29 @@ import requests
 
 import topic_check
 
-
 logger = logging.getLogger(__name__)
 
 
 KSQL_URL = "http://localhost:8088"
 
-#
-# TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
-#       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
-#       `turnstile` table and grouping on station_id.
-#       Make sure to cast the COUNT of station id to `count`
-#       Make sure to set the value format to JSON
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id INT,
+    station_name VARCHAR,
+    line VARCHAR
 ) WITH (
-    ???
+    KAFKA_TOPIC = 'org.chicago.cta.station.turnstile.v1',
+    VALUE_FORMAT = 'avro',
+    KEY = 'station_id'
 );
 
-CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+CREATE TABLE TURNSTILE_SUMMARY
+WITH (VALUE_FORMAT = 'json') AS
+    SELECT station_id, COUNT(station_id) AS count
+    FROM turnstile
+    GROUP BY station_id
+;
 """
 
 
@@ -52,8 +50,15 @@ def execute_statement():
         ),
     )
 
+    json_response = resp.json()
+
     # Ensure that a 2XX status code was returned
-    resp.raise_for_status()
+    try:
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logger.error(json_response)
+    except Exception as e:
+        logger.exception("unknown error occured", e)
 
 
 if __name__ == "__main__":
