b413956e4f3c3c785cc93e0f4b08c2bbbe7193d0
Mahdi Hosseini <mihi2000@gmail.com>
2020-01-04 20:50:57 -0800

    add consumer config

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -7,7 +7,6 @@ from confluent_kafka.avro import AvroConsumer
 from confluent_kafka.avro.serializer import SerializerError
 from tornado import gen
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -30,48 +29,27 @@ class KafkaConsumer:
         self.consume_timeout = consume_timeout
         self.offset_earliest = offset_earliest
 
-        #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
-        # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
         self.broker_properties = {
-                #
-                # TODO
-                #
+            "bootstrap.servers": "PLAINTEXT://localhost:9092",
+            "group.id": self.topic_name_pattern,
+            "default.topic.config": {"auto.offset.reset": "earliest"},
         }
 
-        # TODO: Create the Consumer, using the appropriate type.
         if is_avro is True:
             self.broker_properties["schema.registry.url"] = "http://localhost:8081"
-            #self.consumer = AvroConsumer(...)
+            self.consumer = AvroConsumer(self.broker_properties)
         else:
-            #self.consumer = Consumer(...)
-            pass
+            self.consumer = Consumer(self.broker_properties)
 
-        #
-        #
-        # TODO: Configure the AvroConsumer and subscribe to the topics. Make sure to think about
-        # how the `on_assign` callback should be invoked.
-        #
-        #
-        # self.consumer.subscribe( TODO )
+        self.consumer.subscribe([self.topic_name_pattern], on_assign=self.on_assign)
 
     def on_assign(self, consumer, partitions):
         """Callback for when topic assignment takes place"""
-        # TODO: If the topic is configured to use `offset_earliest` set the partition offset to
-        # the beginning or earliest
-        logger.info("on_assign is incomplete - skipping")
         for partition in partitions:
-            pass
-            #
-            #
-            # TODO
-            #
-            #
+            if self.offset_earliest is True:
+                partition.offset = confluent_kafka.OFFSET_BEGINNING
 
-        logger.info("partitions assigned for %s", self.topic_name_pattern)
+        logger.info(f"partitions assigned for %s", self.topic_name_pattern)
         consumer.assign(partitions)
 
     async def consume(self):
@@ -84,21 +62,31 @@ class KafkaConsumer:
 
     def _consume(self):
         """Polls for a message. Returns 1 if a message was received, 0 otherwise"""
-        #
-        #
-        # TODO: Poll Kafka for messages. Make sure to handle any errors or exceptions.
-        # Additionally, make sure you return 1 when a message is processed, and 0 when no message
-        # is retrieved.
-        #
-        #
-        logger.info("_consume is incomplete - skipping")
-        return 0
+        logger.debug("consuming topic %s", self.topic_name_pattern)
+        try:
+            message = self.consumer.poll(timeout=self.consume_timeout)
+        except SerializerError as e:
+            logger.error("failed to process message", e)
+            return 0
 
+        try:
+            if message is None:
+                logger.debug("no messages retrieved")
+                return 0
+            elif message.error() is not None:
+                logger.error("failed to consume message", message.error())
+                return 0
+            else:
+                self.message_handler(message)
+                return 1
+        except Exception as e:
+            logger.exception("unknown error occured while processing message", e)
+            return 0
+
+        return 0
 
     def close(self):
         """Cleans up any open kafka consumers"""
-        #
-        #
-        # TODO: Cleanup the kafka consumer
-        #
-        #
+
+        logger.debug("gracefully shutting down consumer")
+        self.consumer.close()
